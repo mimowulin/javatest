e765edb785a698d650ab4526786ad7d534e74636
mimo360 <mimo360@gmail.com>
2015-04-05 18:57:33 +0800

    時間測試 Date

--- src/test/ArrayTest.java
+++ src/test/ArrayTest.java
@@ -4,6 +4,21 @@ public class ArrayTest {
 
 	public static void main(String[] args) {
 		int[] array = { 6, 7, 3 };
-		System.out.println(array[1]);
+		System.out.println(array[1] + "單一取出");
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+		System.out.println("強化for");
+		for (int ints : array) {
+			System.out.println(ints);
+		}
+		java.util.Date date = new java.util.Date();
+		System.out.println(date);
+		System.out.println(String.format("%tj", date));
+		System.out.println(String.format("%ta", date));
+		System.out.println(String.format("%tA", date));
+		System.out.println(String.format("%tb", date));
+		System.out.println(String.format("%tB", date));
+		System.out.println(String.format("%tY", date));
 	}
 }
